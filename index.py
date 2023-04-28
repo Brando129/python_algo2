56aeec546e49df7404fde4479a667e2858589c85
Brandon Colangelo <brandoncancode@gmail.com>
2023-04-28 11:40:46 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,11 @@
+# This function takes all the numbers from 0 - a users input number and
+# adds each number to next until the user number is reached.
+
+def get_sum(num):
+    sum = 0
+
+    for i in range(0,num+1):
+        sum += i 
+    return sum
+
+print(get_sum(2))
